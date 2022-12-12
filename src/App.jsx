9bc86afb47380557ada6cd75dd0ed38184a2bd4a
sallanez <saul.alejandro19@gmail.com>
2022-12-12 12:10:00 -0600

    example 3 of ref hook completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import UseRefExample1 from "./components/UseRefExample1.jsx";
 import UseRefExample2 from "./components/UseRefExample2.jsx";
+import UseRefExample3 from "./components/UseRefExample3.jsx";
+
 function App() {
   return (
     <div className="container mt-5">
       <UseRefExample1 />
       <UseRefExample2 />
+      <UseRefExample3 />
     </div>
   );
 }
