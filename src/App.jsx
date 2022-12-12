33bb1a2ab6db0e26189dbfe3db19cdce2af66eba
sallanez <saul.alejandro19@gmail.com>
2022-12-12 11:25:57 -0600

    example 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import UseRefExample1 from "./components/UseRefExample1.jsx";
-
+import UseRefExample2 from "./components/UseRefExample2.jsx";
 function App() {
   return (
     <div className="container mt-5">
       <UseRefExample1 />
+      <UseRefExample2 />
     </div>
   );
 }
